6275361ecdffd2b9d5df0ac795e4cd8bfc2dd324
woozi <kwj1270@naver.com>
2024-05-21 05:47:08 +0900

    test: 미션1 테스트 행위 작성

---
+++ src/test/java/org/woozi/pratice/ReflectionTest.java
@@ -0,0 +1,46 @@
+package org.woozi.pratice;
+
+import org.junit.jupiter.api.Test;
+
+class ReflectionTest {
+
+    @Test
+    void 클래스명를_가져온다() {
+
+    }
+
+    @Test
+    void 필드명을_가져온다() {
+
+    }
+
+    @Test
+    void 생성자정보를_가져온다() {
+
+    }
+
+    @Test
+    void 메소드정보를_가져온다() {
+
+    }
+
+    @Test
+    void test로_시작하는_메소드를_실행한다() {
+
+    }
+
+    @Test
+    void printView_어노테이션을_실행한다() {
+
+    }
+
+    @Test
+    void private_필드에_값을_할당한다() {
+
+    }
+
+    @Test
+    void 인자를_가진_생성자의_인스터스를_생성한다() {
+
+    }
+}
